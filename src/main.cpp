0467d0121691e3f3248cbe75b540241833e431b9
Mason Stuart <mason.stuart05@outlook.com>
2025-02-21 11:57:39 +0000

    optimizations

--- src/main.cpp
+++ src/main.cpp
@@ -14,15 +14,18 @@
 
 int main(int argc, char * argv[])
 {
-	int numberPoints = 60;
+	int numberPoints = 4;
 	if (argc>1)
 	{
 		numberPoints = atoi(argv[1]);
 	}
 
+	int win_w = 2560;
+	int win_h = 1440;
+
 	std::default_random_engine eng(std::random_device{}());
-	std::uniform_real_distribution<double> dist_w(0, 800);
-	std::uniform_real_distribution<double> dist_h(0, 800);
+	std::uniform_real_distribution<double> dist_w(0, win_w);
+	std::uniform_real_distribution<double> dist_h(0, win_h);
 
 	std::cout << "Generating " << numberPoints << " random points" << std::endl;
 
@@ -31,42 +34,42 @@ int main(int argc, char * argv[])
 		points.push_back(dt::Vector2<double>{dist_w(eng), dist_h(eng)});
 	}
 
-	dt::Delaunay<double> triangulation;
-	const auto start = std::chrono::high_resolution_clock::now();
-	const std::vector<dt::Triangle<double>> triangles = triangulation.triangulate(points);
-	const auto end = std::chrono::high_resolution_clock::now();
-	const std::chrono::duration<double> diff = end - start;
+	// dt::Delaunay<double> triangulation;
+	// const auto start = std::chrono::high_resolution_clock::now();
+	// const std::vector<dt::Triangle<double>> triangles = triangulation.triangulate(points);
+	// const auto end = std::chrono::high_resolution_clock::now();
+	// const std::chrono::duration<double> diff = end - start;
 
-	std::cout << triangles.size() << " triangles generated in " << diff.count()
-			<< "s\n";
-	const std::vector<dt::Edge<double>> edges = triangulation.getEdges();
+	// std::cout << triangles.size() << " triangles generated in " << diff.count()
+	// 		<< "s\n";
+	// const std::vector<dt::Edge<double>> edges = triangulation.getEdges();
 
 	// SFML window
-	sf::RenderWindow window(sf::VideoMode(800, 800), "Delaunay triangulation");
-	window.setFramerateLimit(1);
+	sf::RenderWindow window(sf::VideoMode(win_w, win_h), "Delaunay triangulation");
+	window.setFramerateLimit(20);
 
-	std::vector<std::array<sf::Vertex, 2> > lines;
-	for(const auto &e : edges) {
-		const std::array<sf::Vertex, 2> line{{
-			sf::Vertex(sf::Vector2f(
-					static_cast<float>(e.v->x + 2.),
-					static_cast<float>(e.v->y + 2.))),
-			sf::Vertex(sf::Vector2f(
-					static_cast<float>(e.w->x + 2.),
-					static_cast<float>(e.w->y + 2.))),
-		}};
-		window.draw(line.data(), 2, sf::Lines);
-	}
+	// std::vector<std::array<sf::Vertex, 2> > lines;
+	// for(const auto &e : edges) {
+	// 	const std::array<sf::Vertex, 2> line{{
+	// 		sf::Vertex(sf::Vector2f(
+	// 				static_cast<float>(e.v->x + 2.),
+	// 				static_cast<float>(e.v->y + 2.))),
+	// 		sf::Vertex(sf::Vector2f(
+	// 				static_cast<float>(e.w->x + 2.),
+	// 				static_cast<float>(e.w->y + 2.))),
+	// 	}};
+	// 	window.draw(line.data(), 2, sf::Lines);
+	// }
 
 	// Transform each points of each vector as a rectangle
-    for(const auto p : points) {
-		sf::RectangleShape s{sf::Vector2f(4, 4)};
-		s.setPosition(static_cast<float>(p.x), static_cast<float>(p.y));
-        s.setFillColor(sf::Color::Red);
-        s.setOutlineColor(sf::Color::Red);
-        s.setOutlineThickness(3);
-		window.draw(s);
-	}
+    // for(const auto p : points) {
+	// 	sf::RectangleShape s{sf::Vector2f(4, 4)};
+	// 	s.setPosition(static_cast<float>(p.x), static_cast<float>(p.y));
+    //     s.setFillColor(sf::Color::Red);
+    //     s.setOutlineColor(sf::Color::Red);
+    //     s.setOutlineThickness(3);
+	// 	window.draw(s);
+	// }
 
 	window.display();
 
@@ -78,6 +81,48 @@ int main(int argc, char * argv[])
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+
+		points.push_back(dt::Vector2<double>{dist_w(eng), dist_h(eng)});
+
+		window.clear(sf::Color::White);
+
+		dt::Delaunay<double> triangulation;
+		const auto start = std::chrono::high_resolution_clock::now();
+		const std::vector<dt::Triangle<double>> triangles = triangulation.triangulate(points);
+		const auto end = std::chrono::high_resolution_clock::now();
+		const std::chrono::duration<double> diff = end - start;
+	
+		std::cout << triangles.size() << " triangles generated in " << diff.count()
+				<< "s\n";
+		const std::vector<dt::Edge<double>> edges = triangulation.getEdges();
+
+		std::vector<std::array<sf::Vertex, 2> > lines;
+		for(const auto &e : edges) {
+			const std::array<sf::Vertex, 2> line{{
+				sf::Vertex(sf::Vector2f(
+						static_cast<float>(e.v->x + 2.),
+						static_cast<float>(e.v->y + 2.)),
+						sf::Color::Black),
+						
+				sf::Vertex(sf::Vector2f(
+						static_cast<float>(e.w->x + 2.),
+						static_cast<float>(e.w->y + 2.)),
+						sf::Color::Black),
+			}};
+			window.draw(line.data(), 2, sf::Lines);
+		}
+
+		for(const auto p : points) {
+			sf::RectangleShape s{sf::Vector2f(4, 4)};
+			s.setPosition(static_cast<float>(p.x), static_cast<float>(p.y));
+			s.setFillColor(sf::Color::Red);
+			s.setOutlineColor(sf::Color::Red);
+			s.setOutlineThickness(3);
+			window.draw(s);
+		}
+
+
+		window.display();
 	}
 
 	return 0;
